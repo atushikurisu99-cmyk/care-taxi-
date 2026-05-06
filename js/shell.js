9c9c34d88949ae0235246cb38c99d18f3ead8e75
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-05-07 03:51:51 +0900

    Update shell.js

--- js/shell.js
+++ js/shell.js
@@ -1,1 +1,62 @@
-window.AppShell=function(){function render(root){var L=window.AppTexts.labels;root.innerHTML='<div class="header"><div class="menu"><span></span><span></span><span></span></div><div class="date">4/29（火）14:26</div><div class="reserve">'+L.reserve+'</div></div><div class="content"><div class="area-label label-location">'+L.location+'</div><div class="area-label label-detail">'+L.detail+'</div><div class="area-label label-days">'+L.days+'</div><div class="area-label label-status">'+L.status+'</div><div id="location-root" class="location-root"></div><div id="detail-root" class="detail-root"></div><div id="days-root" class="days-root"></div><div id="status-root" class="status-root"></div></div>'}return{render:render}}();
+window.AppShell = (function(){
+
+  function render(root){
+    root.innerHTML = `
+      <div id="care-ui-root" style="
+        position:relative;
+        width:100vw;
+        height:var(--app-h,100vh);
+        background:#ffffff;
+        overflow:hidden;
+        font-family:-apple-system,BlinkMacSystemFont,'Helvetica Neue','Yu Gothic','YuGothic',Meiryo,sans-serif;
+      ">
+
+        <div id="location-root" style="
+          position:absolute;
+          left:48px;
+          top:36px;
+          width:calc(79vw - 48px);
+          height:calc(var(--app-h,100vh) - 250px);
+          overflow:hidden;
+          background:#fff;
+        "></div>
+
+        <div id="detail-root" style="
+          position:absolute;
+          right:48px;
+          top:36px;
+          width:calc(21vw - 20px);
+          height:calc(var(--app-h,100vh) - 250px);
+          overflow:hidden;
+          background:#fff;
+        "></div>
+
+        <div id="days-root" style="
+          position:absolute;
+          left:48px;
+          bottom:36px;
+          width:calc(79vw - 48px);
+          height:170px;
+          overflow:hidden;
+          background:#fff;
+        "></div>
+
+        <div id="status-root" style="
+          position:absolute;
+          right:48px;
+          bottom:36px;
+          width:calc(21vw - 20px);
+          height:170px;
+          overflow:hidden;
+          background:#fff;
+        "></div>
+
+      </div>
+    `;
+  }
+
+  return {
+    render: render
+  };
+
+})();
