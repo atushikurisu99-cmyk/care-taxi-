d1a94e4f8310026d976d8f8b176792f1d7ecf8f7
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-05-09 07:18:35 +0900

    Add files via upload

--- js/days.js
+++ js/days.js
@@ -1,13 +1,60 @@
 (function () {
   var DAYS = [
-    { date: "4/29", body: "案件数 8件<br>調整数 8件" },
-    { date: "4/30", body: "案件数 8件<br>調整数 7件" },
-    { date: "5/1", body: "案件数 8件<br>調整数 8件" },
-    { date: "5/2", body: "案件数 8件<br>調整数 7件" },
-    { date: "5/3", body: '案件数 8件<br>調整数 8件<br><span class="badge blue">請求書発行</span><br><span class="badge blue">&nbsp;</span><br><span class="badge blue">&nbsp;</span>' },
-    { date: "5/4", body: '案件数 8件<br><span style="color:#ff2b50;">調整数 7件</span><br><span class="badge yellow">支払い</span>' },
-    { date: "5/5", body: "案件数 8件<br>調整数 8件" }
+    {
+      date: "4/29",
+      count: "案件数 8件",
+      adjust: "調整数 8件",
+      notices: ["", "", ""]
+    },
+    {
+      date: "4/30",
+      count: "案件数 8件",
+      adjust: "調整数 7件",
+      notices: ["", "", ""]
+    },
+    {
+      date: "5/1",
+      count: "案件数 8件",
+      adjust: "調整数 8件",
+      notices: ["", "", ""]
+    },
+    {
+      date: "5/2",
+      count: "案件数 8件",
+      adjust: "調整数 7件",
+      notices: ["", "", ""]
+    },
+    {
+      date: "5/3",
+      count: "案件数 8件",
+      adjust: "調整数 8件",
+      notices: [
+        '<span class="badge blue">請求書発行</span>',
+        '<span class="badge blue">&nbsp;</span>',
+        '<span class="badge blue">&nbsp;</span>'
+      ]
+    },
+    {
+      date: "5/4",
+      count: "案件数 8件",
+      adjust: '<span style="color:#ff2b50;">調整数 7件</span>',
+      notices: [
+        '<span class="badge yellow">支払い</span>',
+        "",
+        ""
+      ]
+    },
+    {
+      date: "5/5",
+      count: "案件数 8件",
+      adjust: "調整数 8件",
+      notices: ["", "", ""]
+    }
   ];
+
+  function line(content, className) {
+    return '<div class="day-line ' + (className || "") + '">' + (content || "&nbsp;") + '</div>';
+  }
 
   function renderDays() {
     var root = document.getElementById("daysList");
@@ -16,8 +63,15 @@
 
     for (i = 0; i < DAYS.length; i++) {
       html += '<div class="day-card" style="left:' + (i * 91) + 'px;">';
-      html += '<div class="day-date">' + DAYS[i].date + '</div>';
-      html += '<div class="day-body">' + DAYS[i].body + '</div>';
+      html += '<div class="day-grid">';
+      html += line(DAYS[i].date, "date");
+      html += line("&nbsp;");
+      html += line(DAYS[i].count);
+      html += line(DAYS[i].adjust);
+      html += line(DAYS[i].notices[0], "notice");
+      html += line(DAYS[i].notices[1], "notice");
+      html += line(DAYS[i].notices[2], "notice");
+      html += '</div>';
       html += '</div>';
     }
 
