63646d0862698a785a611d4f71824567df355903
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-05-13 08:19:02 +0900

    Delete js directory

--- js/status.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  function render(root,data){
-    root.innerHTML='';
-    data.statuses.forEach(row=>{const div=document.createElement('div');div.className='status-row';div.innerHTML='<b>'+row[0]+'</b><span>'+row[1]+'</span>';root.appendChild(div);});
-  }
-  window.CareTaxiStatus={render};
-})();
