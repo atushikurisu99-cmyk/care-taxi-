7951aac32b43966e23f09fefdd5602b807728bfb
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-05-13 08:18:40 +0900

    Delete care-taxi--main directory

--- care-taxi--main/js/detail.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  function render(root,data){
-    root.innerHTML='';
-    data.detailRows.forEach((t,i)=>{const div=document.createElement('div');div.className=i===data.detailRows.length-1?'detail-line detail-dash':'detail-line';div.textContent=t;root.appendChild(div);});
-  }
-  window.CareTaxiDetail={render};
-})();
